6d9b6384c886d38ebd23c91730b19384fa7ae726
ildeniz <ildeniz.bekaslan@gmail.com>
2023-02-26 16:03:29 +0100

    user info scraper created and implemented in data collection script, user info data uploaded

--- 01_-_data_collection.py
+++ 01_-_data_collection.py
@@ -16,7 +16,10 @@ os.chdir(dir_path)
 
 #%%
 %%time #Wall time: 5min 26s
-from mal_scrapper import seasonal_anime_info
+'''
+Scraping animes between 2010 winter season and 2022 fall season.
+'''
+from mal_scraper import seasonal_anime_info
 import pandas as pd
 
 first = 2010
@@ -32,3 +35,44 @@ for year in years:
         animes = pd.concat([animes, animes_season], ignore_index=True)
         
 animes.to_csv('Data/animes_2010_-_2022.csv', index=False)
+#%%
+%%time
+'''
+Scraping user data for the given animes.
+Hint: Scraping info of 100 animes lasted 1h 33min 6s
+                      1000 animes lasted 16h 13min 19s
+For safe of time efficinecy I recommend to scrap data through multiple cloud slutions 
+in smaller fractions (for example 100 titles at a time) and combine them later.
+'''
+from mal_scraper import user_updates_info
+import pandas as pd
+
+animes_2010_2022 = pd.read_csv('Data/info_animes_2010_-_2022.csv')
+animes = animes_2010_2022['Mal_Id'] #[:100]
+
+user_info = pd.DataFrame()
+animes_scrapped = 0
+for anime in animes:
+    anime_scrapped = user_updates_info(anime)
+    user_info = pd.concat([user_info, anime_scrapped], ignore_index=True)
+    animes_scrapped += 1
+
+user_info.to_csv('Data/users_info_animes_combined.csv', index=False)
+#%%
+# merging multiple files
+import pandas as pd
+import glob
+import os
+
+file_name = 'multi_combo_'
+
+# identify the files to be merged
+joined_files = os.path.join("Data/", f"{file_name}*.csv")
+  
+# list of joined files
+joined_list = glob.glob(joined_files)
+  
+# merging the files
+df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
+
+    
--- mal_scraper.py
+++ mal_scraper.py
@@ -10,16 +10,16 @@ import os
 # Setting the working directory
 path = os.getcwd()
 
-real_path = os.path.realpath('__file__')
+real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
 os.chdir(dir_path)
 #%%
 import pandas as pd
-#from jikanpy import Jikan
 import requests
-import time # jikan API has a 'rate limiting': 30 requests per minute and 2 requests per second
+import time # Jikan API has a 'rate limiting': 30 requests per minute and 2 requests per second
+            # hence we need the execution 'sleep'
 
-def seasonal_anime_info(year, season):
+def seasonal_anime_info(year: int, season: str):
     '''
     Returns anime infos* of the specified season
     year (int) – Year to get anime of.
@@ -27,55 +27,78 @@ def seasonal_anime_info(year, season):
     
     *Title (str), MyAnimeList ID (int), Score (float), Scored_by (int), Members (int), Favourited_by (int), Genre (comma seperated str)
     '''
-    initial_response = requests.get("https://api.jikan.moe/v4/seasons/" + str(year) + "/" + season)
+    
+    initial_url =  f"https://api.jikan.moe/v4/seasons/{year}/{season}"
+    initial_response = requests.get(initial_url)
     season_info = initial_response.json()
     
-    titles = []
-    mal_ids = []
-    scores = []
-    scored_by = []
-    members = []
-    genres = []
-    favorites = []
-    ani_year = []
-    ani_season = []
-    
+    items = []
     for page in range(season_info['pagination']['last_visible_page']):    
-        response = requests.get("https://api.jikan.moe/v4/seasons/" + str(year) + "/" + season + "?page=" + str(page+1))
+        url = f'{initial_url}?page={page+1}'
+        response = requests.get(url)
         season_info = response.json()
         
         time.sleep(1)
         
         for i in range(len(season_info['data'])):
-            if season_info['data'][i]['score'] == None: # excluding animes without score
-                pass
-            else:    
-                titles.append(season_info['data'][i]['title'])
-                mal_ids.append(season_info['data'][i]['mal_id'])
-                scores.append(season_info['data'][i]['score'])
-                scored_by.append(season_info['data'][i]['scored_by'])
-                members.append(season_info['data'][i]['members'])
-                favorites.append(season_info['data'][i]['favorites'])
-                combined_genres=[]
-                for genre in range(len(season_info['data'][i]['genres'])):
-                    combined_genres.append(season_info['data'][i]['genres'][genre]['name'])
-                combined_genres = ','.join(combined_genres)
-                genres.append(combined_genres)
-                ani_year.append(season_info['data'][i]['year'])
-                ani_season.append(season_info['data'][i]['season'])
+            if not season_info['data'][i]['score']: # excluding animes without score
+                continue
+            combined_genres=[]
+            for genre in range(len(season_info['data'][i]['genres'])):
+                combined_genres.append(season_info['data'][i]['genres'][genre]['name'])
+            combined_genres = ','.join(combined_genres)
+            
+            new_item = {'Title': season_info['data'][i]['title'], 
+                        'Mal_Id': season_info['data'][i]['mal_id'], 
+                        'Rating': season_info['data'][i]['score'],
+                        'Scored_By': season_info['data'][i]['scored_by'],
+                        'Members': season_info['data'][i]['members'],
+                        'Favourited_by': season_info['data'][i]['favorites'],
+                        'Genre': combined_genres,
+                        'Year': season_info['data'][i]['year'],
+                        'Season': season_info['data'][i]['season'],
+                       }
+            items.append(new_item)
         
         time.sleep(1)
         
-    anim_data = {'Title': titles, 
-                 'Mal_Id': mal_ids, 
-                 'Rating': scores,
-                 'Scored_By':scored_by,
-                 'Members':members,
-                 'Favourited_by': favorites,
-                 'Genre': genres,
-                 'Year': ani_year,
-                 'Season': ani_season,
-                }
-    df = pd.DataFrame(data = anim_data)
+    df = pd.DataFrame(items)
     return df
 
+def user_updates_info(anime_id: int, pages = 5):
+    '''
+    Returns users' info, who rated a particular anime, from the 'userupdate' page of the anime
+    anime_id (int)
+    pages (int) = 5
+    
+    'User_name' (str), 'Score' (int), 'Status' (str), 'Anime_Id (int)'
+    '''
+
+    initial_url =  f"https://api.jikan.moe/v4/anime/{anime_id}/userupdates"
+    items = []
+    
+    for page in range(5):    
+        url = f'{initial_url}?page={page+1}'
+        response = requests.get(url)
+        time.sleep(1)
+        user_info = response.json()
+        
+        if 'error' in user_info: # avoid "408 Request Timeout" error
+            continue
+        for i in range(len(user_info['data'])):
+            if not user_info['data'][i]['score']: # exclude non-voting users
+                continue
+            new_item = {'User_Name': user_info['data'][i]['user']['username'], 
+                        'Score': user_info['data'][i]['score'],
+                        'Status': user_info['data'][i]['status'],
+                        'Anime_Id': anime_id,
+                       }
+            items.append(new_item)
+            
+    
+    # remove duplicated rows
+    df = pd.DataFrame(items)
+    duplicated_row = df[df.duplicated(keep = 'first')]
+    df.drop(index = duplicated_row.index, axis = 0, inplace = True)
+        
+    return df
